621cb6592ba280e5718fbfc9f664c2ff64aa46a5
spxilt <100957070+spxilt@users.noreply.github.com>
2022-03-04 17:01:01 +0000

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,2 +1,3 @@
+if game.PlaceId == 2917762324 then    
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
     local Window = Library.CreateLib("SpoiltHub, "Midnight")
