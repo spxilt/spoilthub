1807d54f5fb45bbff004c6c34fe783c5044bbb80
spxilt <100957070+spxilt@users.noreply.github.com>
2022-03-15 11:15:47 +0000

    Delete hub.lua

--- hub.lua
+++
@@ -1,3 +0,0 @@
-if game.PlaceId == 2917762324 then    
-    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-    local Window = Library.CreateLib("SpoiltHub, "Midnight")
